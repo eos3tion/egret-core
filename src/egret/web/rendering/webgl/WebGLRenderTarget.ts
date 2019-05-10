4a1e2a8c398e32617fdf9979d0c161c38f179028
程方 <eos_3tion@163.com>
2019-05-10 18:29:55 +0800

    微调写法

--- src/egret/web/rendering/webgl/WebGLRenderTarget.ts
+++ src/egret/web/rendering/webgl/WebGLRenderTarget.ts
@@ -117,11 +117,11 @@ namespace egret.web {
             }
         }
 
-        private createTexture(): WebGLTexture {
+        private createTexture() {
             let gl = this.gl;
 
-            let texture: WebGLTexture = gl.createTexture();
-            texture["glContext"] = gl;
+            let texture = gl.createTexture();
+            texture.glContext = gl;
             gl.bindTexture(gl.TEXTURE_2D, texture);
             gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, this.width, this.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
